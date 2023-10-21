2bdd21b8463ef1d9b4d42a0a895b1f49d0455f0c
saroj-mali <sarojmali6090@gmail.com>
2023-10-22 05:12:57 +0530

    Header completed

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import left from "./Images/left.jpg";
+import logo from './Images/logo4.png';
+import {GiFlowerEmblem,GiFlowerStar,GiSunflower,GiSpotedFlower } from 'react-icons/gi'
+import {IoMdFlower,IoFlowerSharp} from 'react-icons/io';
+import {PiFlowerLotusFill} from 'react-icons/pi'
+ 
+// GiFlowerEmblem GiFlowerStar GiSunflower GiSpotedFlower  PiFlowerLotusFill
+
+function Header() {
+  return (
+    <div className="krishna flex justify-between items-center px-10">
+      <div>
+        <img className="h-28" src={left} alt="flute" />
+      </div>
+      <div className="flex justify-center items-center">
+      <GiSunflower  className="text-5xl text-teal-600"/>
+      </div>
+      <div>
+        <img className="h-28" src={logo} alt="logo"/>
+      </div>
+      <div className="flex justify-center items-center">
+      {/* <GiFlowerEmblem className="text-3xl text-green-600"/>
+        <GiFlowerStar  className="text-4xl text-pink-600"/> */}
+        <GiSunflower  className="text-5xl text-teal-600"/>
+        {/* <GiSpotedFlower   className="text-3xl text-pink-600"/>
+        <IoMdFlower  className="text-3xl text-purple-600"/>
+        <PiFlowerLotusFill   className="text-3xl text-red-600"/> */}
+
+
+      </div>
+      <div><img className="h-28 -rotate-180" src={left} alt="flute" /></div>
+    </div>
+  );
+}
+
+export default Header;
